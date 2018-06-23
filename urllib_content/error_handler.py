e2d268ec59aa430c5653c9d7e0f8d5ae8b609f22
fubst0318 <fubst0318@126.com>
2018-06-23 08:30:09 +0800

    enviroment setting

--- urllib_content/error_handler.py
+++ urllib_content/error_handler.py
@@ -1,5 +1,13 @@
 #!/usr/bin/env python
 # -*- encoding: utf-8 -*-
 
-if __name__ =='__main__':
-    print('test')
+from urllib.request import urlopen
+from urllib.error import URLError
+import socket
+
+if __name__ == '__main__':
+    try:
+        rep = urlopen('http://httpbin.org/get', timeout=0.1)
+    except URLError as e:
+        if isinstance(e.reason, socket.timeout):
+            print('Time out!')
