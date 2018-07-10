bc25a5efd18c6ae49e8176708dc21ebcff7232d3
fubst0318 <fubst0318@126.com>
2018-07-10 14:00:12 +0800

    mulitiprocessing 起步

---
+++ multiprocessing_content/mulpStart.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+# -*- encoding: utf-8 -*-
+
+'''
+这是一个multiprocessing的模块开始模块
+'''
+
+from multiprocessing import Pool
+
+
+def f(x):
+    return x * x
+
+
+if __name__ == '__main__':
+    with Pool(5) as p:
+        print(p.map(f, [1, 2, 3]))
