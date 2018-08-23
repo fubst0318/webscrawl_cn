c12d92239f4900c320b6218640a993fadcbd3133
fubst0318 <fubst0318@126.com>
2018-08-23 10:11:09 +0800

    免费代理线程池构建第一步

---
+++ proxy_content/ProxyPool/setting.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+# -*- encoding: utf-8 -*-
+
+# Redis数据库地址
+REDIS_HOST = 'localhost'
+
+# Redis端口
+REDIS_PORT = 6379
+
+# Redis密码，如无填None
+REDIS_PASSWORD = None
+
+REDIS_KEY = 'proxies'
+
+# 代理分数
+MAX_SCORE = 100
+MIN_SCORE = 0
+INITIAL_SCORE = 10
