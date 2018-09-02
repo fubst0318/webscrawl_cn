03c438e586742db1186884741fa5903cd4713500
fubst0318 <fubst0318@126.com>
2018-09-02 10:25:38 +0800

    爬虫爬取微信 更新

---
+++ proxy_content/SogouWeixin/weixin/spider.py
@@ -0,0 +1,160 @@
+#!/usr/bin/env python
+# -*- encoding: utf-8 -*-
+
+from requests import Session
+from db import RedisQueue
+from request import WeixinRequest
+from urllib.parse import urlencode
+import requests
+from pyquery import PyQuery as pq
+from requests import ReadTimeout, ConnectionError
+from config import PROXY_POOL_URL, VALID_STATUSES, MAX_FAILED_TIME
+from mysql import MySQL
+
+
+class Spider():
+    base_url = 'http://weixin.sogou.com/weixin'
+    keyword = 'NBA'
+    headers = {
+        'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8',
+        'Accept-Encoding': 'gzip, deflate',
+        'Accept-Language': 'zh-CN,zh;q=0.9',
+        'Cache-Control': 'max-age=0',
+        'Connection': 'keep-alive',
+        'Cookie': 'IPLOC=CN3100; SUV=006058CA7258092E5A365AE5C31C4119; usid=7owwSvanBFp34s-d; ABTEST=8|1535070343|v1; SUID=4AFC5A653F18960A000000005B7F5087; SUID=4AFC5A652E18960A000000005B7F5087; weixinIndexVisited=1; SNUID=76C7665E3B39481850181AC33C52A601; sct=3; JSESSIONID=aaaDu_x19Td39bcXoOBvw',
+        'Host': 'weixin.sogou.com',
+        'Upgrade-Insecure-Requests': '1',
+        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/68.0.3440.106 Safari/537.36'
+    }
+    session = Session()
+    queue = RedisQueue()
+    mysql = MySQL()
+
+    def get_proxy(self):
+        """
+        从代理池获取代理
+        :return: 
+        """
+        try:
+            response = requests.get(PROXY_POOL_URL)
+            if response.status_code == 200:
+                print('Get Proxy', response.text)
+                return response.text
+        except requests.ConnectionError:
+            return None
+
+    def start(self):
+        """
+        初始化工作
+        """
+        # 全局更新Headers
+        self.session.headers.update(self.headers)
+        start_url = self.base_url + '?' + \
+            urlencode({'query': self.keyword, 'type': 2})
+        weixin_request = WeixinRequest(
+            url=start_url, callback=self.parse_index, need_proxy=True)
+        # 调度第一个请求
+        self.queue.add(weixin_request)
+
+    def parse_index(self, response):
+        """
+        解析索引页
+        :param response:响应
+        :return: 新的响应
+        """
+        doc = pq(response.text)
+        items = doc('.news-box .news-list li .txt-box h3 a').items()
+        for item in items:
+            url = item.attr('href')
+            weixin_request = WeixinRequest(url=url, callback=self.parse_detail)
+            yield weixin_request
+        next = doc('#sogou_next').attr('href')
+        if next:
+            url = self.base_url + str(next)
+            weixin_request = WeixinRequest(
+                url=url, callback=self.parse_index, need_proxy=True)
+            yield weixin_request
+
+    def parse_detail(self, response):
+        """
+        解析详情页
+        :param responose:响应
+        :return: 微信公众号文章
+        """
+        doc = pq(response.text)
+        data = {
+            'title': doc('.rich_media_title').text(),
+            'content': doc('.rich_media_content').text(),
+            'date': doc('#post-date').text(),
+            'nickname': doc('#js_profile_qrcode > div > strong').text(),
+            'wechat': doc('#js_profile_qrcode > div > p:nth-child(3) > span').text()
+        }
+        yield data
+
+    def error(self, weixin_request):
+        """
+        错误处理
+        :param weixin_request:请求
+        :return:
+        """
+        weixin_request.fail_time = weixin_request.fail_time + 1
+        print('Request Failed', weixin_request.fail_time,
+              'Times', weixin_request.url)
+        if weixin_request.failtime < MAX_FAILED_TIME:
+            self.queue.add(weixin_request)
+
+    def schedule(self):
+        """
+        调度请求
+        :return:
+        """
+        while not self.queue.empty():
+            weixin_request = self.queue.pop()
+            callback = weixin_request.callback
+            print('Schedule', weixin_request.url)
+            response = self.request(weixin_request)
+            if response and response.status_code in VALID_STATUSES:
+                results = list(callback(response))
+                if results:
+                    for result in results:
+                        print('New Result', type(result))
+                        if isinstance(result, WeixinRequest):
+                            self.queue.add(result)
+                        if isinstance(result, dict):
+                            self.mysql.insert('articles', result)
+                else:
+                    self.error(weixin_request)
+            else:
+                self.error(weixin_request)
+
+    def request(self, weixin_request):
+        """
+        执行请求
+        :param weixin_request：请求
+        :return: 响应
+        """
+        try:
+            if weixin_request.need_proxy:
+                proxy = self.get_proxy()
+                if proxy:
+                    proxies = {
+                        'http': 'http://' + proxy,
+                        'https:': 'https://' + proxy
+                    }
+                    return self.session.send(weixin_request.prepare(), timeout=weixin_request.timeout, allow_redirects=False, proxies=proxies)
+            return self.session.send(weixin_request.prepare(), timeout=weixin_request.timeout, allow_redirects=False)
+        except (ConnectionError, ReadTimeout) as e:
+            print(e.args)
+            return False
+
+    def run(self):
+        """
+        入口
+        :return:
+        """
+        self.start()
+        self.schedule()
+
+if __name__ =='__main__':
+    spider = Spider()
+    spider.run()
