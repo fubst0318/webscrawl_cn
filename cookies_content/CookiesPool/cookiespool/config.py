712d2d58197ec4c0a08344bbe9da5aa006d1f4bd
fubst0318 <fubst0318@126.com>
2018-09-05 14:16:59 +0800

    cookies池调度完成

--- cookies_content/CookiesPool/cookiespool/config.py
+++ cookies_content/CookiesPool/cookiespool/config.py
@@ -19,6 +19,24 @@ GENERATOR_MAP = {
 }
 
 # 测试类，如扩展其他站点，请在此配置
+TESTER_MAP = {
+    'weibo': 'WeiboValidTester'
+}
+
 TEST_URL_MAP = {
     'weibo': 'https://m.weibo.cn/'
 }
+
+# 产生器和验证器循环周期
+CYCLE = 120
+
+# API地址和端口
+API_HOST = '0.0.0.0'
+API_PORT = 5000
+
+# 产生器开关，模拟登录添加Cookies
+GENERATOR_PROCESS = False
+# 验证器开关，循环检测数据库中Cookies是否可用，不可用删除
+VALID_PROCESS = False
+# API接口服务
+API_PROCESS = True
